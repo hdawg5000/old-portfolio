8e60305602f923e06d4fb15f6c867ad25ef94e40
Hammad Naseem <hammadnsm@gmail.com>
2016-06-29 12:28:09 -0500

    Updated portfolio layout, updated CSS and updated jQuery animations for header

--- js/title.js
+++ js/title.js
@@ -1,5 +1,5 @@
 $(function () {
     $(".name").hide().css("color", "white").delay(1000).fadeIn(3000);
-    $(".delay").delay(4000).fadeIn(3000);
-    $(".intro-text li a").hide().css("color", "white").delay(7000).fadeIn(1000);
+    $(".delay").delay(3000).fadeIn(3000);
+    $(".intro-text li a").hide().css("color", "white").delay(5000).fadeIn(1000);
 });
