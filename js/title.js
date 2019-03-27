f76a45aac69bb645ed048f84da2926c698b86883
Hammad Naseem <hammadnsm@gmail.com>
2019-03-26 19:29:50 -0500

    Removed jquery library

--- js/title.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-    $(".name").hide().css("color", "white").delay(200).fadeIn(2000);
-    $(".delay").delay(1000).fadeIn(2000);
-    $(".intro-text li a").hide().css("color", "white").delay(2000).fadeIn(1000);
-});
